53ff7d35ef1d10b4982c31998fd81bfa94cbad91
William Imoukhuede <wimoukhuede@gmail.com>
2021-01-15 14:11:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render(){
+        return(
+            <div className='App'>
+                <Navigation />
+                {/* {<Logo />
+                <ImageLinkForm />
+                <FaceRecognition />} */}
+            </div>
+        );
+    }
+}
+
+export default App;

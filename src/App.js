6ce6106c0f08658f70bffac823235c3f7eb88006
William Imoukhuede <wimoukhuede@gmail.com>
2021-01-18 17:47:09 +0100

    Face recognition component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Rank from './Components/Rank/Rank';
 import  particular from './Particles.js';
+
+const app =new Clarifai.App({
+    apiKey: 'ff1d290814384e9b92df1908a2d9f269'
+});
 
 const particlesOptions = particular;
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            input:'',
+        }
+    }
+
+    onInputChange = (event) => {
+        console.log(event.target.value);        
+    }
+
+    onButtonSubmit = () => {
+        console.log('click');
+        const texxx= app.models.initModel('d02b4508df58432fbb84e800597b8959');
+        console.log('tired', texxx);
+        app.models.predict( "a403429f2ddf4b49b307e318f00e528b" ,"https://models.com/newfaces/dailyduo/47554").then(
+            function(response) {
+                console.log('works' ,response);
+            },
+            function(err){
+                console.log('error oo', err);
+            }
+        );
+    }
+
     render(){
         return(
             <div className='App'>
@@ -17,8 +48,11 @@ class App extends Component {
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />                
-                {/* <FaceRecognition /> */}
+                <ImageLinkForm 
+                    onInputChange={this.onInputChange} 
+                    onButtonSubmit={this.onButtonSubmit}
+                />                
+                <FaceRecognition />
             </div>
         );
     }
---
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () =>{
+    return(
+        <div className='center'>
+            <img src='https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8cmFuZG9tfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' alt=''/>
+        </div>
+    );
+}
+
+export default FaceRecognition;

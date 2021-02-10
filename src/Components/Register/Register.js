57329745bd19a9381ddd660e69e15ced0758d3f6
William Imoukhuede <wimoukhuede@gmail.com>
2021-02-10 13:37:37 +0100

    changes made for intergration purposes

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
     }
 
     onSubmitRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://hidden-waters-11020.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://hidden-waters-11020.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
